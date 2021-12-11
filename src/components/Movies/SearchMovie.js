e7a88502f6bcc6a2fc732d65092736b5e4f6c520
Alexander Zharkov <alexander.06.zharkov@gmail.com>
2021-12-12 00:44:30 +0600

    MUI text field for searching

--- src/components/Movies/SearchMovie.js
+++ src/components/Movies/SearchMovie.js
@@ -1,14 +1,68 @@
-const SearchMovie = ({ value, handler }) => {
+import { TextField } from "@material-ui/core";
+import styled from "styled-components";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = (theme) => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+
+  cssLabel: {
+    color: "white",
+    fontSize: "1.2rem",
+  },
+
+  cssOutlinedInput: {
+    "&$cssFocused $notchedOutline": {
+      borderColor: `white !important`,
+    },
+  },
+
+  cssFocused: {
+    color: "white",
+    fontSize: "1.2rem",
+  },
+
+  notchedOutline: {
+    borderWidth: "1px",
+    borderColor: "white !important",
+  },
+});
+
+const SearchMovie = ({ value, handler, classes }) => {
   return (
     <div className="search-box">
-      <input
-        className="search-field"
+      <TextField
+        id="standard-name"
+        label="Type movie name"
+        className={classes.textField}
         value={value}
         onChange={handler}
-        placeholder="Type to search"
+        margin="normal"
+        variant="outlined"
+        fullWidth
+        InputLabelProps={{
+          classes: {
+            root: classes.cssLabel,
+            focused: classes.cssFocused,
+          },
+        }}
+        InputProps={{
+          classes: {
+            root: classes.cssOutlinedInput,
+            focused: classes.cssFocused,
+            notchedOutline: classes.notchedOutline,
+          },
+          inputMode: "numeric",
+        }}
       />
     </div>
   );
 };
 
-export default SearchMovie;
+export default withStyles(styles)(SearchMovie);
